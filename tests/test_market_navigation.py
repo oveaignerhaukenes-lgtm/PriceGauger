ddb044c28f0d43a23b6727067fe7091a0e304661
oveaignerhaukenes-lgtm <ove.aigner.haukenes@gmail.com>
2026-08-09 11:20:40 +0200

    Test app-shell market card routes

--- tests/test_market_navigation.py
+++ tests/test_market_navigation.py
@@ -1,6 +1,6 @@
 from market_navigation import market_detail_href
 
 
-def test_market_detail_href_routes_to_selected_market():
-    assert market_detail_href("Gold") == "/Forecast_Learning?market=Gold"
-    assert market_detail_href("Natural Gas") == "/Forecast_Learning?market=Natural%20Gas"
+def test_market_detail_href_routes_through_app_shell():
+    assert market_detail_href("Gold") == "/?open_market=Gold"
+    assert market_detail_href("Natural Gas") == "/?open_market=Natural%20Gas"
