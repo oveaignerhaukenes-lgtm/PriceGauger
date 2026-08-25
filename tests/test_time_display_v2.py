492c17b2f3b6edd580b123cfe1c4329dfefeee51
oveaignerhaukenes-lgtm <ove.aigner.haukenes@gmail.com>
2026-08-25 19:45:38 +0200

    Tests: cover Plotly Norwegian-time localization

--- tests/test_time_display_v2.py
+++ tests/test_time_display_v2.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timezone
 
-from time_display_v2 import oslo_chart_time, oslo_label, oslo_time
+import plotly.graph_objects as go
+
+from time_display_v2 import localize_plotly_figure_v2, oslo_chart_time, oslo_label, oslo_time
 
 
 def test_oslo_display_uses_summer_time_in_august():
@@ -16,3 +18,17 @@ def test_oslo_chart_time_is_naive_local_clock_for_plotly():
     local = oslo_chart_time(value)
     assert local.hour == 19
     assert local.tzinfo is None
+
+
+def test_plotly_localizer_converts_trace_clock_and_axis_label_without_mutating_source_data():
+    fig = go.Figure(go.Scatter(
+        x=["2026-08-25T17:00:00+00:00"],
+        y=[1.0],
+        hovertemplate="%{x|%H:%M} UTC<extra></extra>",
+    ))
+    fig.update_xaxes(title_text="Tid · UTC")
+    localize_plotly_figure_v2(fig)
+    assert fig.data[0].x[0].hour == 19
+    assert fig.data[0].x[0].tzinfo is None
+    assert "norsk tid" in fig.data[0].hovertemplate
+    assert fig.layout.xaxis.title.text == "Tid · norsk tid"
