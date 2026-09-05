6c2139636d1d6db1fef08695af4d2b20af5ce124
oveaignerhaukenes-lgtm <ove.aigner.haukenes@gmail.com>
2026-09-05 20:12:18 +0200

    Spring Trade Engine: initialize package

---
+++ spring_trade_engine/__init__.py
@@ -0,0 +1,8 @@
+"""Spring Trade Engine bounded context.
+
+Observational/model research only. This package has no Saxo execution authority.
+"""
+
+SPRING_ENGINE_SCHEMA_VERSION = 1
+
+__all__ = ["SPRING_ENGINE_SCHEMA_VERSION"]
