85d96c878368277f8937d02802a792eebca7dec1
oveaignerhaukenes-lgtm <ove.aigner.haukenes@gmail.com>
2026-09-05 22:09:34 +0200

    TradingDesk UI: add layout package

---
+++ tradingdesk_ui/layout/__init__.py
@@ -0,0 +1,21 @@
+from tradingdesk_ui.layout.responsive import (
+    MOBILE_BREAKPOINT_PX,
+    TRADINGDESK_ANALYSIS_SECTION_KEY,
+    TRADINGDESK_AUTOMANAGER_SECTION_KEY,
+    TRADINGDESK_CONTROLS_WIDTH_SETTING_KEY,
+    TRADINGDESK_LIVE_SECTION_KEY,
+    TRADINGDESK_RESPONSIVE_SHELL_KEY,
+    render_tradingdesk_responsive_foundation_v1,
+    tradingdesk_responsive_columns_v1,
+)
+
+__all__ = [
+    "MOBILE_BREAKPOINT_PX",
+    "TRADINGDESK_ANALYSIS_SECTION_KEY",
+    "TRADINGDESK_AUTOMANAGER_SECTION_KEY",
+    "TRADINGDESK_CONTROLS_WIDTH_SETTING_KEY",
+    "TRADINGDESK_LIVE_SECTION_KEY",
+    "TRADINGDESK_RESPONSIVE_SHELL_KEY",
+    "render_tradingdesk_responsive_foundation_v1",
+    "tradingdesk_responsive_columns_v1",
+]
