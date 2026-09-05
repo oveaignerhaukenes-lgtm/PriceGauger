82380148f1e754867b3ce2ae81b4d99e41130a34
oveaignerhaukenes-lgtm <ove.aigner.haukenes@gmail.com>
2026-09-05 22:10:01 +0200

    TradingDesk UI: add chart presentation package

---
+++ tradingdesk_ui/charts/__init__.py
@@ -0,0 +1,11 @@
+from tradingdesk_ui.charts.profile import (
+    RESPONSIVE_CHART_PROFILE_V1,
+    ResponsiveChartProfileV1,
+    responsive_chart_profile_payload_v1,
+)
+
+__all__ = [
+    "RESPONSIVE_CHART_PROFILE_V1",
+    "ResponsiveChartProfileV1",
+    "responsive_chart_profile_payload_v1",
+]
