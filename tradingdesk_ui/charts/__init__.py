ab07508ba50625b26ecc6a7bb025ceb0ac22d927
oveaignerhaukenes-lgtm <ove.aigner.haukenes@gmail.com>
2026-09-05 22:13:04 +0200

    TradingDesk UI: expose responsive chart runtime

--- tradingdesk_ui/charts/__init__.py
+++ tradingdesk_ui/charts/__init__.py
@@ -3,9 +3,11 @@ from tradingdesk_ui.charts.profile import (
     ResponsiveChartProfileV1,
     responsive_chart_profile_payload_v1,
 )
+from tradingdesk_ui.charts.responsive_runtime import render_tradingdesk_responsive_runtime_v1
 
 __all__ = [
     "RESPONSIVE_CHART_PROFILE_V1",
     "ResponsiveChartProfileV1",
+    "render_tradingdesk_responsive_runtime_v1",
     "responsive_chart_profile_payload_v1",
 ]
