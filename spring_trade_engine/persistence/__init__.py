550feedd31ff36cc394fea7582301f37d3314f94
oveaignerhaukenes-lgtm <ove.aigner.haukenes@gmail.com>
2026-09-05 20:15:08 +0200

    Spring Trade Engine: establish persistence namespace

---
+++ spring_trade_engine/persistence/__init__.py
@@ -0,0 +1,7 @@
+from spring_trade_engine.persistence.store import (
+    MODEL_VERSION,
+    ensure_spring_schema_v1,
+    persist_spring_observation_v1,
+)
+
+__all__ = ["MODEL_VERSION", "ensure_spring_schema_v1", "persist_spring_observation_v1"]
