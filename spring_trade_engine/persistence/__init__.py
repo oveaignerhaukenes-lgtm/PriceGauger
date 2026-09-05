073405960889a0374e101cfb9124a4b1d6c1348b
oveaignerhaukenes-lgtm <ove.aigner.haukenes@gmail.com>
2026-09-05 20:29:47 +0200

    Spring: expose evaluation persistence

--- spring_trade_engine/persistence/__init__.py
+++ spring_trade_engine/persistence/__init__.py
@@ -1,7 +1,29 @@
+from spring_trade_engine.persistence.evaluation_store import (
+    EVALUATION_VERSION,
+    ensure_spring_evaluation_schema_v1,
+    load_pending_forward_label_seeds_v1,
+    load_spring_observations_v1,
+    persist_episode_candidate_v1,
+    persist_forward_label_v1,
+    persist_runtime_coverage_v1,
+    persist_turning_point_v1,
+)
 from spring_trade_engine.persistence.store import (
     MODEL_VERSION,
     ensure_spring_schema_v1,
     persist_spring_observation_v1,
 )
 
-__all__ = ["MODEL_VERSION", "ensure_spring_schema_v1", "persist_spring_observation_v1"]
+__all__ = [
+    "EVALUATION_VERSION",
+    "MODEL_VERSION",
+    "ensure_spring_evaluation_schema_v1",
+    "ensure_spring_schema_v1",
+    "load_pending_forward_label_seeds_v1",
+    "load_spring_observations_v1",
+    "persist_episode_candidate_v1",
+    "persist_forward_label_v1",
+    "persist_runtime_coverage_v1",
+    "persist_spring_observation_v1",
+    "persist_turning_point_v1",
+]
